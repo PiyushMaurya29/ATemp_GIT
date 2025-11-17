10ad82ffde915923baaed82d4781ffc444fdb501
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-11-17 13:46:12 +0530

    "TEM"

---
+++ NodeJS/hotelModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const hotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+const Hotel = mongoose.model('Hotel', hotelSchema);
+module.exports = Hotel;
