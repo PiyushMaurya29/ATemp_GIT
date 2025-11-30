599a93de5c0b4489daf2366c4ba091bc6f0b7bb7
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-12-01 00:02:15 +0530

    YYYY

--- GIT/temp.js
+++ GIT/temp.js
@@ -1,13 +1,5 @@
-let a = "djdj";
-console.log(a);
+console.log("Hello, World!");
 
-let b = 42;
-console.log(b);
-
-
-function greet(name) {
-    return `Hello, ${name}!`;
-}console.log(greet("World"));
-
-let c = [1, 2, 3, 4, 5];
-console.log(c);
+function add(a, b) {
+    return a + b;
+}
