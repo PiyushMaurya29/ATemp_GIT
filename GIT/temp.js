4b81b915733ba3b069d0286bf19d47f95a7769d1
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-12-05 19:03:32 +0530

    YUP

--- GIT/temp.js
+++ GIT/temp.js
@@ -1,12 +1,4 @@
-const fs = require("fs");
-fs.writeFileSync("temp.txt", "This is a temporary file NEW LOL.");
-console.log("Temporary file created.");
+const os = require("os");
 
-setTimeout(() => {
-  fs.unlinkSync("temp.txt");
-  console.log("Temporary file deleted.");
-}, 5000);
+console.log(os.cpus().length);
 
-// This script creates a temporary file and deletes it after 5 seconds.
-
-// To run this script, use the command: node temp.js
