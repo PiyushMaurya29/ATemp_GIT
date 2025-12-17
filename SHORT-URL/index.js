556db620b16527b67a5937b25cd5bf282d6d3728
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-12-17 23:01:13 +0530

    djhd

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -1,12 +1,3 @@
-
-
-let a = 10;
-let b = 20;
-
-let sum = a + b;
-
-console.log("The sum of a and b is: " + sum);
-
-module.exports = {
-  sum: sum
-};  
+let a = 'Hello, ';
+let b = 'world!';
+console.log(a + b);
