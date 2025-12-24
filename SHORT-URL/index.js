073c3bd4c18fd053f5870b8e1b65d371a30d4ee8
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-12-24 22:40:56 +0530

    jdjdjd

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 
 fs.writeFileSync('output.txt', 'Hello, World!', 'utf8');
 console.log('File written successfully.');
+
+
+let data = fs.readFileSync('output.txt', 'utf8');
+console.log('File content:', data);
