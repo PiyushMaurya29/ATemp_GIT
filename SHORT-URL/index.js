41821c4c5564e1674296ff25dc3602729c219f98
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-12-24 22:41:24 +0530

    HDHDH

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -6,3 +6,6 @@ console.log('File written successfully.');
 
 let data = fs.readFileSync('output.txt', 'utf8');
 console.log('File content:', data);
+
+fs.unlinkSync('output.txt');
+console.log('File deleted successfully.');
