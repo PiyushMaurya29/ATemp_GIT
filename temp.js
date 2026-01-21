4c7aa06b90044d0634e15ced5f3979468b6a3c38
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2026-01-21 11:03:34 +0530

    :DDJJD

--- temp.js
+++ temp.js
@@ -5,5 +5,8 @@ function greet(name) {
     return `Hello, ${name}!`;
 } 
 console.log(greet('Alice'));
+console.log(greet('Bob'));
 
-export { greet };
+let numbers = [1, 2, 3, 4, 5];
+let squaredNumbers = numbers.map(num => num * num);
+console.log(squaredNumbers);
