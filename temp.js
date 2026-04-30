481b6529ab5f7f757f5eccf0c0ca47033cbf8d31
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2026-05-01 01:54:01 +0530

    "YUP"

--- temp.js
+++ temp.js
@@ -10,3 +10,10 @@ console.log(result2); // 30
 
 const result3 = addNumbers(10, 20, 30);
 console.log(result3); // 60
+
+function printStringLetters(str) {
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+  }
+}
+
