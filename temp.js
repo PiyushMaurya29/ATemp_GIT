c9e1ef219062e1fbc5ddf14b750340e4eedb2182
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2026-05-01 01:54:25 +0530

    "Deleted"

--- temp.js
+++ temp.js
@@ -8,12 +8,3 @@ function addNumbers(a, b, c) {
 const result2 = addNumbers(10, 20);
 console.log(result2); // 30
 
-const result3 = addNumbers(10, 20, 30);
-console.log(result3); // 60
-
-function printStringLetters(str) {
-  for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);
-  }
-}
-
